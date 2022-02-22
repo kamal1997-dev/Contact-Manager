9aba38a2458125633d80ab2d948f2c363b762cdc
kamal1997-dev <75772527+kamal1997-dev@users.noreply.github.com>
2022-02-22 17:01:38 +0530

    Redeployed

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const jsonServer=require('json-server')
+const jsonServer=require('./Server/node_modules/json-server')
 
 const server=jsonServer.create();
 const router=jsonServer.router('./Server/db.json')
 const middlewares=jsonServer.defaults({
     static:"./build"
 })
-const port=process.env.PORT||5000;
+const port=process.env.PORT||9000;
 server.use(middlewares);
 server.use(
     jsonServer.rewriter({
-        "/api/*":"$1"
+        "/api/*":"/$1"
     })
 )
 server.use(router)
