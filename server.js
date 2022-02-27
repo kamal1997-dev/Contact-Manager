da7c02a4f0ae2164ce7ee0fc8c95ceda9cc22170
kamal1997-dev <75772527+kamal1997-dev@users.noreply.github.com>
2022-02-27 15:18:28 +0530

    killed

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const jsonServer=require('./Server/node_modules/json-server')
+const jsonServer = require('./Server/node_modules/json-server');
+const server = jsonServer.create();
 
-const server=jsonServer.create();
-const router=jsonServer.router('./Server/db.json')
-const middlewares=jsonServer.defaults({
-    static:"./build"
-})
-const port=process.env.PORT||9000;
-server.use(middlewares);
-server.use(
-    jsonServer.rewriter({
-        "/api/*":"/$1"
-    })
-)
-server.use(router)
-server.listen(port,()=>{
-    console.log(`server is running on ${port}`)
-})
+const path = require('path');
+const express = require('express');
+
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('./Server/db.json');
+const port = process.env.PORT || 4000;
+
+server.use('./Server/db', middlewares, router);
+server.use(express.static(path.join(__dirname, 'build')));
+
+server.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+server.listen(port,()=>{console.log('hi kamal')});
